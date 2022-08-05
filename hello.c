09f73bb49f65efa7945dd4fee7c9aec40dccef60
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-05 10:33:04 +0700

    first commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a = 10 ;
+int main()
+{
+    
+    system("cls");
+    printf("Abid Juliant Indraswara");
+    printf("\nMagelang");
+}
---
+++ tutorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  // Create variables
+  int myNum = 5;
+  float myFloatNum = 5.99;
+  char myLetter = 'D';
+  int a = 20;
+  int b = 21;
+
+  if (a > b){
+    printf("a is greater than b\n");
+  } else {
+    printf("b is greater than a\n");
+  }
+  // Print variables
+  printf("My favourite number is : %d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  printf("Hello World!");
+  return 0;
+}
