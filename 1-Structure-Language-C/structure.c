6d51d56086822bc6dba106c6fcb5f0a80a12115a
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-08 09:16:05 +0700

    Strcture Code

---
+++ 1-Structure-Language-C/structure.c
@@ -0,0 +1,9 @@
+#include<stdio.h>   // Call Library
+
+void main(){        // Function Declaration
+    int A, B, T;    // Init Variable
+    A = 2;          // Variable A
+    B = 3;          // Variable B
+    T = A + B;      // Variable T
+    printf("%i", T);// Print the T Variable
+}
