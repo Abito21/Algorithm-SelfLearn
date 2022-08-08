771cc127a686abd5ba617b9a22f010198b3ff939
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-08 15:35:29 +0700

    Update Structure-2

---
+++ 1-Structure-Language-C/structure-2.c
@@ -0,0 +1,10 @@
+// Puts Integer from String
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+    char kar[8] = "2756.5AB";
+    int Nilai;
+    Nilai = atoi(kar);
+    printf("%i", Nilai);
+}
