a9ec083a4b0e8d040f6c3e0136e60aa620711977
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-08 09:57:02 +0700

    Update Structure #2

--- 1-Structure-Language-C/structure.c
+++ 1-Structure-Language-C/structure.c
@@ -1,9 +1,12 @@
 #include<stdio.h>   // Call Library
+#include<math.h>
 
 void main(){        // Function Declaration
-    int A, B, T;    // Init Variable
+    int A, B, T;    // Init Integer Variable
+    float C;        // Init Float Variable
     A = 2;          // Variable A
-    B = 3;          // Variable B
+    B = 5;          // Variable B
+    C = sqrt(B);    // Variable C
     T = A + B;      // Variable T
-    printf("%i", T);// Print the T Variable
+    printf("Nilai Total T : %i dan Akar Kuadrat B : %18.16f", T, C);    // Print the T Variable
 }
