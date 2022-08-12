a7f9dbd9a77cf42717bb83d5107b7b800fe23a28
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-12 16:30:31 +0700

    Init CodeBlocks Folder

---
+++ CodeBlocks/ujicoba.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+    int a, b, c;
+    a = 23;
+    b = 7;
+    c = a + b;
+    printf("%i\n", c);
+    printf("My Name is Abid and my favourite number is : %i\n", b);
+    printf("Hello, World!");
+}
