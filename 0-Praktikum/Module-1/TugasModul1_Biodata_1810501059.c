3cd662a083afbf7dc0f5457bbf75111071809998
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-13 12:10:23 +0700

    Tugas Modul 1

---
+++ 0-Praktikum/Module-1/TugasModul1_Biodata_1810501059.c
@@ -0,0 +1,17 @@
+/* ------------------------------------
+        Tugas Modul 1 Biodata
+   ------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    printf("=========== Biodata ===========\n");
+    printf(" Nama : Abid Juliant Indraswara\n");
+    printf(" NPM  : 1810501059\n");
+    printf("------------- Info ------------\n");
+    printf(" Kelahiran : Magelang\n");
+    printf(" Sekolah : SMAN 4 Magelang\n");
+    printf(" Hobi : Memancing\n");
+    printf("===============================");
+}
