fd586926c65f3a0bcea772b8998e3fd79f01dd3e
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-13 12:02:49 +0700

    Init C programming learn

---
+++ 0-Praktikum/Module-1/Modul_1_ProgramPertamaku.c
@@ -0,0 +1,13 @@
+/* ------------------------------------
+        Progran Pertamaku
+   ------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    // Menampilkan hasil outpuy
+    printf("Hello, World!\n");
+
+    return 0;
+}
