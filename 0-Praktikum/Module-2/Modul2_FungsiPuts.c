6e9752b3ab5da18156e6b71c0d1257f313deb394
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-13 12:50:02 +0700

    Modul 2 - Fungsi puts

---
+++ 0-Praktikum/Module-2/Modul2_FungsiPuts.c
@@ -0,0 +1,15 @@
+/* ------------------------------------
+        Modul 2 - Fungsi puts
+   ------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    // Menggunakan puts
+    puts("Kampus    : Universitas Tidar");
+    puts("Jurusan   : Teknik Elektro");
+    puts("Tahun     : 2022");
+
+    return 0;
+}
