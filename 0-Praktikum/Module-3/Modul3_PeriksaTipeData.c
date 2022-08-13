fc38b496b4bf8e04fc9f433966dc9d7b5c21da58
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-13 18:42:47 +0700

    Modul 3 - Periksa Tipe Data

---
+++ 0-Praktikum/Module-3/Modul3_PeriksaTipeData.c
@@ -0,0 +1,35 @@
+/* ------------------------------------
+        Modul 3 Variabel, Tipe Data dan Operator - Periksa Tipe Data
+   ------------------------------------
+*/
+
+#include<stdio.h>
+#include<limits.h>
+
+int main(){
+    printf("Memeriksa Memori Tipe Data \n");
+    printf("Char    : %d byte \n", sizeof(char));
+    printf("Short   : %d byte \n", sizeof(short));
+    printf("Int     : %d byte \n", sizeof(int));
+    printf("Long    : %d byte \n", sizeof(long));
+    printf("------------------------------------\n");
+    printf("\n");
+
+    printf("Memeriksa Nilai Minimal Tipe Data \n");
+    printf("Char    : %d \n", SCHAR_MIN);
+    printf("Short   : %d \n", SHRT_MIN);
+    printf("Int     : %d \n", INT_MIN);
+    printf("Long    : %d \n", LONG_MIN);
+    printf("------------------------------------\n");
+    printf("\n");
+
+    printf("Memeriksa Nilai Maksimal Tipe Data \n");
+    printf("Char    : %d \n", SCHAR_MAX);
+    printf("Short   : %d \n", SHRT_MAX);
+    printf("Int     : %d \n", INT_MAX);
+    printf("Long    : %d \n", LONG_MAX);
+    printf("------------------------------------\n");
+    printf("\n");
+
+    return 0;
+}
