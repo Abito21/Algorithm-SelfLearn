9260ba2ece6133d6ffafbb0aefe842cd5d952b46
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-13 12:53:06 +0700

    Modul 2 - Fungsi putchar

---
+++ 0-Praktikum/Module-2/Modul2_FungsiPutchar.c
@@ -0,0 +1,14 @@
+/* ------------------------------------
+        Modul 2 - Fungsi putchar
+   ------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    // Menggunakan putchar
+    putchar('L');
+    putchar('7');
+
+    return 0;
+}
