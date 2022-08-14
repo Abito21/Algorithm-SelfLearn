2dbcd63e1ac3aa38ca3778215dce1f2fafa5640e
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-14 16:39:32 +0700

    Update Modul 3 - OPperator Logika

--- 0-Praktikum/Module-3/Modul3_OperatorLogika.c
+++ 0-Praktikum/Module-3/Modul3_OperatorLogika.c
@@ -16,7 +16,7 @@ int main(){
     printf("(15 <= 15) && (15 < 15): %d \n", x);
     x = ('a' == 'a') || ('a' == 'b');
     printf("('a' == 'a') || ('a' == 'b'): %d \n", x);
-    x = (10 > 7) && ('Sapu' == 'sapun');
+    x = (10 > 7) && ("Sapu" == "sapun");
     printf("(10 > 7) && ('Sapu' == 'sapun'): %d \n", x);
 
     return 0;
