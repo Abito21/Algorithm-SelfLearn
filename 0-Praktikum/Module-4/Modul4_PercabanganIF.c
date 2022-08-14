4556a94dd9335dd5229dc6d808a7205dd644f437
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-14 19:38:13 +0700

    Modul 4 - Percabangan IF

---
+++ 0-Praktikum/Module-4/Modul4_PercabanganIF.c
@@ -0,0 +1,25 @@
+/* ------------------------------------
+        Tugas Modul 4 - Percabangan IF
+        Judul : Cek Suu
+   ------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    // Deklarasi Variabel
+    float suhu;
+
+    // Membuat input
+    printf("Inputkan Suhu : ");
+    scanf("%f", &suhu);
+
+    // Cek Suhu dengan percabangan IF
+    if(suhu > 36){
+        printf("\n----------------------------\n");
+        printf("Suhu saat inii : %.2f \n", suhu);
+        printf("Suhu Terlalu Tinggi \n");
+        printf("----------------------------\n");
+    }
+    return 0;
+}
