2ad9afea3704d69f58093af90badf2627cc6992e
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-17 13:25:10 +0700

    Modul 6 - For

---
+++ 0-Praktikum/Module-6/Modul6_For.c
@@ -0,0 +1,38 @@
+/* -------------------------------------------
+        Modul 6 - Perulangan For
+   -------------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    // Case 1
+    for(int i = 0; i < 10; i++){
+        printf("Perulangan ke-%i \n", i);
+    }
+
+    // Case 2
+    /*
+    for(int counter = 0; counter < 50; counter += 2){
+        printf("Perulangan ke-%i \n", counter);
+    }
+    */
+    
+    // Case 3
+    /*
+    for(int counter = 10; counter > 0;  counter --){
+        printf("Perulangan ke-%i \n", counter);
+    }
+    */
+
+   // Case 4
+   /*
+   int i;
+   for(i = 1; i <= 10; i++){
+    printf("%i", i*3);
+   }
+   printf("\n");
+   */
+
+    return 0;
+}
