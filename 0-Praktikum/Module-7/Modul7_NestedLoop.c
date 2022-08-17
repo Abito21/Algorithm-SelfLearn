6d351574b2b334a31040ede843b904448ed1448c
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-17 14:41:40 +0700

    Modul 7 - Perulangan Bertingkat

---
+++ 0-Praktikum/Module-7/Modul7_NestedLoop.c
@@ -0,0 +1,16 @@
+/* -------------------------------------------
+        Modul 7 - Perulangan Bertingkat
+   -------------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            printf("Perulangan ke (%d, %d) \n", i, j);
+        }
+    }
+    
+    return 0;
+}
