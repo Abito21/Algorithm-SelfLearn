78872efd743638fe8f4a5e5ce8a02f9955a2dda5
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-20 13:57:51 +0700

    Modul 8 - Array 1 Dimens

---
+++ 0-Praktikum/Module-8/Modul8_Array1D.c
@@ -0,0 +1,19 @@
+/* -------------------------------------------
+        Modul 8 - Array 1 Dimensi
+   -------------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    // Deklarasi variabel
+    // Tipe Data Integer
+    int A[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // Tipe Data String atau Character
+    char B[5] = {'A', 'B', 'C', 'D', 'E'};
+
+    printf("%d %d %d %d %d %d %d %d %d %d \n", A[0], A[1], A[2], A[3], A[4], A[5], A[6], A[7], A[8], A[9]);
+    printf("%c %c %c %c %C \n", B[0], B[1], B[2], B[3], B[4]);
+
+    return 0;
+}
