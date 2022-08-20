3e6f1c88b03ff9b13dcc729b83d3dbca1cc88dc1
Abid Indraswara <67644383+Abito21@users.noreply.github.com>
2022-08-20 14:15:14 +0700

    Modul 9 - Array 2 Dimensi

---
+++ 0-Praktikum/Module-9/Modul9_Array2D.c
@@ -0,0 +1,22 @@
+/* -------------------------------------------
+        Modul 9 - Array 2 Dimensi
+   -------------------------------------------
+*/
+
+#include<stdio.h>
+
+int main(){
+    int angka[2][2] = {75,83, 
+                       92,77};
+    
+    char item[3][10] = {"buku", 
+                        "pensil", 
+                        "meja"};
+
+    printf("%d %d \n", angka[0][0], angka[0][1]);
+    printf("%d %d \n", angka[1][0], angka[1][1]);
+    printf("\n");
+    // printf("%s \n", item[0][0]);
+    // printf("%s \n", item[1][0]);
+    // printf("%s \n", item[2][0]);
+}
