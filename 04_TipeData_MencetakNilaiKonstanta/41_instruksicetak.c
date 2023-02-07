f7879ddd8e8c03de8aaabe25736f51ea915a6645
Abito21 <abidjuliant@gmail.com>
2023-02-07 08:52:43 +0700

    instruksi cetak

---
+++ 04_TipeData_MencetakNilaiKonstanta/41_instruksicetak.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+    int A, B, T;
+    A = 5;
+    B = 2;
+    T = A + B;
+    printf("%i", T);    // printf merupakan fungsi dari library stdio.h untuk menampilkan variabel T (dengan tipe data integer) dalam bentuk string.
+}
