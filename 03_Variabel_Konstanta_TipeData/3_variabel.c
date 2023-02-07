8573437d2ce6d49694706367828a51466e123ca7
Abito21 <abidjuliant@gmail.com>
2023-02-07 08:44:35 +0700

    variabel

---
+++ 03_Variabel_Konstanta_TipeData/3_variabel.c
@@ -0,0 +1,13 @@
+#include<stdio.h> # Library
+
+void main(){
+    /*
+        - int disebut sebagai tipe data integer
+        - A / B / T disebut sebagai variabel.
+    */
+    int A, B, T;
+    A = 5;              // Variabel A berisi nilai 5
+    B = 2;              // Variabel B berisi nilai 2
+    T = A + B;          // Variabel T merupakan penjumlahan dari nilai variabel A dan nilai variabel B
+    printf("%i", T);    // printf merupakan fungsi dari library stdio.h untuk menampilkan variabel T (dengan tipe data integer) dalam bentuk string.
+}
