7fb9509a9987c1b14e2c439d55f1a6759d9957d3
Abito21 <abidjuliant@gmail.com>
2023-02-09 09:31:52 +0700

    atu posisi cetakan layar

--- 04_TipeData_MencetakNilaiKonstanta/46_aturPosisiCetakanLayar.c
+++ 04_TipeData_MencetakNilaiKonstanta/46_aturPosisiCetakanLayar.c
@@ -1,2 +1,25 @@
 #include<stdio.h>
 #include<conio.h>
+
+// gotoxy() function
+/*
+void gotoxy(int x,int y)
+{
+printf("%c[%d;%df",0x1B,y,x);
+}
+*/
+
+void main(){
+    /*
+    int X, Y, I, J;
+    for (I = 0; I <= 10; J++)
+    {
+        for (J = I; J <= 10; J++)
+        {
+            gotoxy(I, J); printf("*" );
+        }
+    }
+    */
+    gotoxy(10,15);
+    printf("Jakarta");
+}
