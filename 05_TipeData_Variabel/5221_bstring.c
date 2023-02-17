95d49b99d0be363da0f615e67d96b81afad478c4
Abito21 <abidjuliant@gmail.com>
2023-02-17 07:50:25 +0700

    string B

---
+++ 05_TipeData_Variabel/5221_bstring.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+void main(){
+    int N;
+    char *S = "Jakarta";
+    printf("%s", S);
+    printf("\n%c", S[2]);
+    N = strlen(S);
+    printf("\n%i", N);
+}
